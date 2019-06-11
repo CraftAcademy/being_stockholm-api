3a938299c83f5c71924b7ebae503d5ad247694c4
zanenkn <zane.neikena@gmail.com>
2019-06-11 19:58:29 +0200

    Creates Pings Controller, opens the index route and namespaces it, adds configuration in rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
