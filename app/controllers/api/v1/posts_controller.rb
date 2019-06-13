79d551ca8d13eb0a6808882102a93b036ee6789e
Carrosen <carla1.rosen@gmail.com>
2019-06-13 14:59:38 +0200

    Creates test for successfully creating a post via post request. Adds create method in Posts controller, opens route for create action, 21/21 tests passing

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::PostsController < ApplicationController
     posts = Post.all
     render json: posts, each_serializer: Posts::IndexSerializer
   end
+
+  def create
+    post = Post.create(post_params)
+    if post.persisted?
+      render json: { message: 'Successfully created', id: post.id }
+    else
+      render json: { error: post.errors.full_messages }, status: 422
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :status, :category, :longitude, :latitude, keys: [:image])
+  end 
+ 
 end
--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -30,4 +30,33 @@ let(:headers) { { HTTP_ACCEPT: 'application/json' } }
       end
     end
   end
+
+  describe "POST /api/v1/posts" do
+
+
+    describe "successfully" do
+      before do
+        post "/api/v1/posts", params: {
+          post: {
+            image: ('spec/fixtures/dummy_image.jpg'),
+            caption: 'Lorem ipsum dolor',
+            status: 'pending',
+            category: 'work',
+            longitude: 53.06,
+            latitude: 18.03,
+            }
+        }, headers: headers
+      end
+
+      it "creates a new post" do
+        expect(json_response["message"]).to eq "Successfully created"
+        expect(response.status).to eq 200
+      end
+
+      it "sends back into the response the newly created post information" do
+        post = Post.last
+        expect(json_response["id"]).to eq post.id
+      end 
+    end
+  end
 end
