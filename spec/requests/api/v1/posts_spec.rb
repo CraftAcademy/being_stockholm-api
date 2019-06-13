89ba5cb34f78c923edecb555f069a69afa7f3b55
felix_bonnier <felix_bonnier@yahoo.com>
2019-06-13 16:38:56 +0200

    adds request spec for sad path, testing missing fields as well as length for caption to not be longer than 140 characters. 22/22 tests passing

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -33,7 +33,6 @@ let(:headers) { { HTTP_ACCEPT: 'application/json' } }
 
   describe "POST /api/v1/posts" do
 
-
     describe "successfully" do
       before do
         post "/api/v1/posts", params: {
@@ -58,5 +57,44 @@ let(:headers) { { HTTP_ACCEPT: 'application/json' } }
         expect(json_response["id"]).to eq post.id
       end 
     end
+
+    describe "POST /api/v1/posts" do
+
+      describe "unsuccessfully" do
+        it "can not be created without all fields filled in" do
+          post "/api/v1/posts", params: {
+          post: {
+            image: ('spec/fixtures/dummy_image.jpg'),
+            category: 'work',
+            longitude: 53.06,
+            latitude: 18.03,
+            }
+        }, headers: headers
+  
+          expect(json_response['error']).to eq ["Caption can't be blank"]
+          expect(response.status).to eq 422
+        end
+
+        it "can not be created if caption is more than 140 characters" do
+
+          post "/api/v1/posts", params: {
+            post: {
+              image: ('spec/fixtures/dummy_image.jpg'),
+              caption: "Zanes hair is the most beautiful hair I have ever seen!!!!!Zanes hair is the most beautiful hair I have ever seen!!!!!Zanes hair is the most beautiful hair I have ever seen!!!!!Zanes hair is the most beautiful hair I have ever seen!!!!!Zanes hair is the most beautiful hair I have ever seen!!!!!",
+              category: 'work',
+              longitude: 53.06,
+              latitude: 18.03,
+              }
+          }, headers: headers
+
+          expect(json_response['error']).to eq ["Caption is too long (maximum is 140 characters)"]
+        end
+      end
+    end
   end
 end
+
+
+
+
+  
