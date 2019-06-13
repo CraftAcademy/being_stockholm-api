c771946ddda9cc0c2bf1c3117e173f887ee2b0dc
Carrosen <carla1.rosen@gmail.com>
2019-06-13 10:29:09 +0200

    Creates post controller with index method, creates index serialixer for posts with attributes: id, status, longitude, latitude, adds route for index under new namespace(v1)

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
     end
+
+    namespace :v1 do
+      resources :posts, only: [:index]
+    end
   end
 end
