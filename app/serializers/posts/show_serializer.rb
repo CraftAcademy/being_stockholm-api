79e3eff8a629a197dbef5fa21815d1ace4e26dfa
raptorf1 <raptor_f1@hotmail.com>
2019-06-15 19:39:58 +0200

    Adds do-end block to the PostShow serializer. Image information is now attached to the post.

--- app/serializers/posts/show_serializer.rb
+++ app/serializers/posts/show_serializer.rb
@@ -1,7 +1,8 @@
 class Posts::ShowSerializer < ActiveModel::Serializer
   attributes :id, :status, :caption, :category, :longitude, :latitude, :created_at, :image
-  #has_one_attached :image, 
-  def image
-    Post.with_attached_image.where(image: object)
+  
+  has_one_attached :image do |post|
+    post.image
   end
+  
 end
