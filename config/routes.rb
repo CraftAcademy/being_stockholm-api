c9a6d2adc2765ae0941c580c51d803dd2732755a
stefan_karlberg <stefan.karlberg@smackit.se>
2019-06-16 13:46:43 +0200

    Adds routes for devise token auth undr correct name space. 64/64 green

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  
   namespace :api do
     namespace :v0 do
       resources :pings, only: [:index], constraints: { format: 'json' }
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
     namespace :v1 do
       resources :posts, only: [:index, :create]
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
     end
   end
 end
