5e0bd2c6fba371b5ba23ec01f4e24658d1f15e5f
zanenkn <zane.neikena@gmail.com>
2019-06-17 11:48:43 +0200

    Adjusts the show request spec to accomodate the recent changes, refactoring in the Posts controller.

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    post = Post.find(params[:id])
     render json: post, serializer: Posts::ShowSerializer
   end
 
--- spec/requests/api/v1/posts/show_spec.rb
+++ spec/requests/api/v1/posts/show_spec.rb
@@ -1,18 +1,22 @@
 RSpec.describe Api::V1::PostsController, type: :request do
-
-  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
-  let(:image) {
-    {type: "image/png",
-    encoder: "name=carbon (5).png;base64",
-    data: "iVBORw0KGgoAAAANSUhEUgAABjAAAAOmCAYAAABFYNwHAAAgAElEQVR4XuzdB3gU1cLG8Te9EEgISQi9I71KFbBXbFixN6zfvSiIjSuKInoVFOyIDcWuiKiIol4Q6SBVOtI7IYSWBkm+58y6yW4a2SS7O4n/eZ7vuWR35pwzvzO76zf",
-    extension: "png" }
-  }  
+let(:headers) { { HTTP_ACCEPT: 'application/json' } }
   
   describe "GET /api/v1/posts/id" do
     before do
-      post = FactoryBot.create(:post)
-      DecodeImageService.attach_image(image, post.image)
-      get "/api/v1/posts/"+"#{post.id}", headers: headers
+      post "/api/v1/posts", params: {
+        image: {
+          type: "image/png",
+          encoder: "name=carbon (5).png;base64",
+          data: "iVBORw0KGgoAAAANSUhEUgAABjAAAAOmCAYAAABFYNwHAAAgAElEQVR4XuzdB3gU1cLG8Te9EEgISQi9I71KFbBXbFixN6zfvSiIjSuKInoVFOyIDcWuiKiIol4Q6SBVOtI7IYSWBkm+58y6yW4a2SS7O4n/eZ7vuWR35pwzvzO76zf",
+          extension: "png" 
+        },
+        caption: 'Lorem ipsum dolor',
+        category: 'work',
+        longitude: 53.06,
+        latitude: 18.03,
+      }, 
+      headers: headers
+      get "/api/v1/posts/"+"#{Post.last.id}", headers: headers
     end
     
     it "returns a success response" do
@@ -21,14 +25,11 @@ RSpec.describe Api::V1::PostsController, type: :request do
 
     it "returns a specific post" do
       post = Post.last
-      expect(json_response['post']['id']).to eq post.id
+      expect(json_response['id']).to eq post.id
     end
 
-    it "returns the attached image in the response" do
-      post = Post.last
-      attached_image = "iVBORw0KGgoAAAANSUhEUgAABjAAAAOmCAYAAABFYNwHAAAgAElEQVR4Xuzd\nB3gU1cLG8Te9EEgISQi9I71KFbBXbFixN6zfvSiIjSuKInoVFOyIDcWuiKiI\nol4Q6SBVOtI7IYSWBkm+58y6yW4a2SS7O4n/eZ7vuWR35pwzvzO76zcK\n"
-      expect(json_response['image']).to eq attached_image
+    it "returns image object in the response" do      
+      expect(json_response).to include 'image'
     end
-
   end  
 end
